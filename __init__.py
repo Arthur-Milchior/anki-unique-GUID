ebad9a7920cca5a9474a1faae91991f6402d241a
Arthur Milchior <Arthur@Milchior.fr>
2020-03-29 16:05:57 +0200

    pep8

--- __init__.py
+++ __init__.py
@@ -4,21 +4,24 @@ from anki.utils import guid64
 from anki.lang import _
 from aqt.qt import QAction, QKeySequence
 
+
 def ensure():
     mw.checkpoint("Unique GUID")
     lastGuid = None
     nids = []
     lastNid = None
-    for guid, nid in mw.col.db.all("select guid, id from notes order by guid"):
+    for guid, nid in mw.col.db.all("select guid, id from notes order by guid, id"):
         if lastGuid == guid:
             mw.col.modSchema(True)
-            mw.col.db.execute("update notes set guid = ? where id = ? ", guid64(), nid)
-            nids.append((nid,lastNid))
+            mw.col.db.execute(
+                "update notes set guid = ? where id = ? ", guid64(), nid)
+            nids.append((nid, lastNid))
         lastGuid = guid
         lastNid = nid
     if nids:
         s = _("The guid of the following notes have been changed:")
-        s+= "\n".join(_("Note %d which was a copy of note %d")%(nid, lastNid) for nid, lastNid in nids)
+        s += "\n".join(_("Note %d which was a copy of note %d") %
+                       (nid, lastNid) for nid, lastNid in nids)
         showWarning(s)
     else:
         tooltip(_("Each guid was unique. No correction required"))
@@ -28,4 +31,3 @@ action = QAction(mw)
 action.setText("Ensure unique GUID")
 mw.form.menuTools.addAction(action)
 action.triggered.connect(ensure)
-
